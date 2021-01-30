dcc4753c3ce0ab6995473cf52f04e9fa7cdbaf62
Spencer <scrose34@gmail.com>
2021-01-29 23:31:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Deliverable_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an Integer value between 1 and 100 to execute an iterative statement: ");
+            try
+            {
+                string input = Console.ReadLine();
+                int value_of_input = int.Parse(input);
+                if ((value_of_input > 0) && (value_of_input <= 100))
+                {
+                    Console.WriteLine("Executing a For Loop!");
+                    Console.WriteLine("The For Loop will iterate " + value_of_input.ToString() + " times.");
+                    for (int i = 0; i < value_of_input; i++)
+                    {
+                        Console.WriteLine("The value of the variable i in this iteration is: " + i.ToString());
+                    }
+                    Console.WriteLine("Press any key to exit the program");
+                    Console.ReadKey(true);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an integer between 1 and 100 and try again ...");
+                    Console.WriteLine("Press any key to exit the program and try again ...");
+                    Console.ReadKey(true);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please restart the program, and try an integer between 1 and 100 ");
+                Console.WriteLine("Press any key to exit the program ...");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
